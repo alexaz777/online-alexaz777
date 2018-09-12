c88339a586b6f248caa9d8075088db92bd54974c
alexaz777 <a.zadvinsky@gmail.com>
2018-09-12 15:39:34 +0300

    Добавил изменения в task_4_1.py

--- 04_data_structures/task_4_1.py
+++ 04_data_structures/task_4_1.py
@@ -9,4 +9,12 @@
 
 '''
 
+#NAT = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+
+#Solution
 NAT = 'ip nat inside source list ACL interface FastEthernet0/1 overload'
+d = NAT.replace ('Fast' , 'Gigabit')
+print (d)
+print ('End of programm")
+
+ 
