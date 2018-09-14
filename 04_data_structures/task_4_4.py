c8235338ce17fa0537081a11cbfb33146536975e
alexaz777 <a.zadvinsky@gmail.com>
2018-09-14 16:02:24 +0300

    Сделал задание task_4_4.py

--- 04_data_structures/task_4_4.py
+++ 04_data_structures/task_4_4.py
@@ -14,3 +14,17 @@
 
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+
+#Решение
+command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
+command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+d1 = command1.split()
+d2 = command2.split()
+vlans1 = d1[-1].split(',')
+vlans2 = d2[-1].split(',')
+vlans1_set = set(int(vlan) for vlan in vlans1)
+vlans2_set = set(int(vlan) for vlan in vlans2)
+list(vlans1_set & vlans2_set)
+print(list(vlans1_set & vlans2_set)) 
+ 
+
