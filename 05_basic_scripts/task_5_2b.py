79d3acc45fccaf9d8354d1aafb22ed009b6aefa9
alexaz777 <a.zadvinsky@gmail.com>
2018-09-24 16:10:53 +0300

    Сделал задание task_5_2-5_2d.py

---
+++ 05_basic_scripts/task_5_2b.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 5.2b
+
+Переделать скрипт из задания 5.2a таким образом, чтобы, при запросе параметра,
+отображался список возможных параметров.
+
+Вывести информацию о соответствующем параметре, указанного устройства.
+
+Пример выполнения скрипта:
+$ python task_5_2b.py
+Enter device name: r1
+Enter parameter name (ios, model, vendor, location, ip): ip
+10.255.0.1
+
+Ограничение: нельзя изменять словарь london_co.
+
+Все задания надо выполнять используя только пройденные темы.
+То есть эту задачу можно решить без использования условия if.
+'''
+
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+#Решение
+a = input('Enter device name: ')
+print('Enter parameter name' + ' (' + ','.join(list(london_co[a].keys())) + '): ')
+a1 = input()
+print(london_co[a][a1])
---
+++ 05_basic_scripts/task_5_2d.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 5.2d
+
+Переделать скрипт из задания 5.2c таким образом, чтобы, при запросе параметра,
+пользователь мог вводить название параметра в любом регистре.
+
+Пример выполнения скрипта:
+$ python task_5_2d.py
+Enter device name: r1
+Enter parameter name (ios, model, vendor, location, ip): IOS
+15.4
+
+
+Ограничение: нельзя изменять словарь london_co.
+
+Все задания надо выполнять используя только пройденные темы.
+То есть эту задачу можно решить без использования условия if.
+'''
+
+london_co = {
+    'r1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.1'
+    },
+    'r2': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '4451',
+        'ios': '15.4',
+        'ip': '10.255.0.2'
+    },
+    'sw1': {
+        'location': '21 New Globe Walk',
+        'vendor': 'Cisco',
+        'model': '3850',
+        'ios': '3.6.XE',
+        'ip': '10.255.0.101',
+        'vlans': '10,20,30',
+        'routing': True
+    }
+}
+#Решение
+try:
+    a = input('Enter device name: ')
+    print('Enter parameter name' + ' (' + ','.join(list(london_co[a].keys())) + '): ')
+    a1 = input().lower()
+    print(london_co[a][a1])
+except KeyError:
+    print("Такаго параметра нет")
