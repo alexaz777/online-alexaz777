622eb31e12082d0472b915e07c97f2efe18f97f5
alexaz777 <a.zadvinsky@gmail.com>
2019-01-25 16:23:00 +0300

    Сделал задания task_9_1-1a

---
+++ 09_functions/task_9_1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 9.1
+
+Создать функцию, которая генерирует конфигурацию для access-портов.
+
+Функция ожидает, как аргумент, словарь access-портов, вида:
+    { 'FastEthernet0/12':10,
+      'FastEthernet0/14':11,
+      'FastEthernet0/16':17,
+      'FastEthernet0/17':150 }
+
+Функция должна возвращать список всех портов в режиме access
+с конфигурацией на основе шаблона access_template.
+Заготовка для функции уже сделана.
+
+В конце строк в списке не должно быть символа перевода строки.
+
+Пример итогового списка (перевод строки после каждого элемента сделан для удобства чтения):
+[
+'interface FastEthernet0/12',
+'switchport mode access',
+'switchport access vlan 10',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+'interface FastEthernet0/17',
+'switchport mode access',
+'switchport access vlan 150',
+'switchport nonegotiate',
+'spanning-tree portfast',
+'spanning-tree bpduguard enable',
+...]
+
+Проверить работу функции на примере словаря access_dict.
+
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+'''
+#Решение
+access = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17
+}
+access_dict = {
+    'FastEthernet0/12': 10,
+    'FastEthernet0/14': 11,
+    'FastEthernet0/16': 17,
+    'FastEthernet0/17': 150
+}
+
+def generate_access_config(access):
+    access_template = [
+        'switchport mode access', 'switchport access vlan',
+        'switchport nonegotiate', 'spanning-tree portfast',
+        'spanning-tree bpduguard enable'
+    ]
+    for	intf, vlan	in	access.items():
+        print('interface '	+	intf)
+        for	command	in	access_template:
+            if	command.endswith('access vlan'):
+                print('	{}	{}'.format(command,	vlan))
+            else:
+                print('	{}'.format(command))
+
