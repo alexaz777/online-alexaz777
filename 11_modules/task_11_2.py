07de806400395f1f249cfb25b1227c87c7e18797
alexaz777 <a.zadvinsky@gmail.com>
2019-02-19 17:10:16 +0300

    Сделал задание task_11_2

---
+++ 11_modules/task_11_2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 11.2
+
+С помощью функции parse_cdp_neighbors из задания 11.1
+и функции draw_topology из файла draw_network_graph.py
+сгенерировать топологию, которая соответствует выводу
+команды sh cdp neighbor в файле sw1_sh_cdp_neighbors.txt
+
+Не копировать код функций parse_cdp_neighbors и draw_topology.
+
+В итоге, должен быть сгенерировано изображение топологии.
+Результат должен выглядеть так же, как схема в файле task_11_2_topology.svg
+
+При этом:
+* Интерфейсы могут быть записаны с пробелом Fa 0/0 или без Fa0/0.
+* Расположение устройств на схеме может быть другим
+* Соединения должны соответствовать схеме
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+> Для выполнения этого задания, должен быть установлен graphviz:
+> apt-get install graphviz
+
+> И модуль python для работы с graphviz:
+> pip install graphviz
+
+'''
+#Решение
+from task_11_1 import parse_cdp_neighbors
+from draw_network_graph import draw_topology
+
+file_name = '/home/python/online-alexaz777/11_modules/sw1_sh_cdp_neighbors.txt'
+with open(file_name, 'r') as f:
+    sh_cdp_neighbors = f.readlines()
+
+draw_topology(parse_cdp_neighbors(sh_cdp_neighbors), '/home/python/online-alexaz777/11_modules/img/task_11_2_topology')
+
+
+
