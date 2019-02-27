e85bebea26efae9afe37d102b20ee621b9b68a38
alexaz777 <a.zadvinsky@gmail.com>
2019-02-27 15:09:37 +0300

    Сделал задание task_12_1.py

---
+++ 12_useful_modules/task_12_1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 12.1
+
+Создать функцию check_ip_addresses, которая проверяет доступность IP-адресов.
+
+Функция ожидает как аргумент список IP-адресов.
+И возвращает два списка:
+* список доступных IP-адресов
+* список недоступных IP-адресов
+
+Для проверки доступности IP-адреса, используйте ping.
+Адрес считается доступным, если на три ICMP-запроса пришли три ответа.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+#Решение
+import subprocess
+
+
+list_ip_addresses = ['192.168.9.4', "192.168.9.7", '192.168.9.8', '192.168.9.14', '192.168.9.16']
+print('List_ip_addreses' + str(list_ip_addresses))
+Alive = []
+Unreachable =[]
+
+def check_ip_addresses(list_ip_addresses):
+    for ip_addr in list_ip_addresses:
+        reply = subprocess.run(['ping', '-c', '3', '-n', ip_addr], stdout= subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if reply.returncode == 0:
+            Alive.append(ip_addr)
+        else:
+            Unreachable.append(ip_addr)
+    print('Alive' + str(Alive))
+    print('Unreachable' + str(Unreachable))
+
+check_ip_addresses(list_ip_addresses)
