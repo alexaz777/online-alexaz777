ff03c7e89b3f1169a033aad5566358fb628feef6
alexaz777 <a.zadvinsky@gmail.com>
2019-03-01 16:19:28 +0300

    Сделал задание task_12_2.py

--- 12_useful_modules/task_12_2.py
+++ 12_useful_modules/task_12_2.py
@@ -30,28 +30,40 @@ IP-адреса могут быть в формате:
 Для выполнения задачи можно воспользоваться функцией check_ip_addresses из задания 12.1
 '''
 #Решение
-list_ip_addresses = ['217.195.65.9-11', "195.144.224.2", '192.168.9.14-192.168.9.19']
-for ip_addr in list_ip_addresses:
-    if '-' in ip_addr:
-        if ip_addr.count('.') == 3:
-            a = ip_addr.split('.')
-            a3 = a[3].split('-')
-            list1 = [str(a[0] + '.' + a[1] + '.' + a[2] + '.') + str(i) for i in range(int(a3[0]), int(a3[1])+1)]
-            list_ip_addresses.remove(ip_addr)
-            list_ip_addresses = list_ip_addresses + list1
-        elif ip_addr.count('.') == 6:
-            b = ip_addr.split('-')
-            b1 = b[0].split('.')
-            b2 = b[1].split('.')
-            list2 = [str(b1[0] + '.' + b1[1] + '.' + b1[2] + '.') + str(i) for i in range(int(b1[3]), int(b2[3]) +1)]
-            list_ip_addresses.remove(ip_addr)
-            list_ip_addresses = list_ip_addresses + list2
-    else:
-        continue
+import subprocess
 
-from task_12_1 import check_ip_addresses
+list_ip_addresses = ['217.195.65.9-11', '195.144.224.2', '192.168.9.14 - 192.168.9.20']
 
-result = check_ip_addresses(list_ip_addresses)
-print('List_ip_addreses' + str(list_ip_addresses))
-print('Alive'+ str(result[0]))
-print('Unreachable' + str(result[1]))
+def check_ip_availability(list_ip_addresses):
+    for ip_addr in list_ip_addresses:
+        if '-' in ip_addr:
+            if ip_addr.count('.') == 3:
+                a = ip_addr.split('.')
+                a3 = a[3].split('-')
+                list1 = [str(a[0] + '.' + a[1] + '.' + a[2] + '.') + str(i) for i in range(int(a3[0]), int(a3[1])+1)]
+                list_ip_addresses.remove(ip_addr)
+                list_ip_addresses = list_ip_addresses + list1
+            elif ip_addr.count('.') == 6:
+                b = ip_addr.split('-')
+                b1 = b[0].split('.')
+                b2 = b[1].split('.')
+                list2 = [str(b1[0] + '.' + b1[1] + '.' + b1[2] + '.') + str(i) for i in range(int(b1[3]), int(b2[3]) +1)]
+                list_ip_addresses.remove(ip_addr)
+                list_ip_addresses = list_ip_addresses + list2
+        else:
+            continue
+    Alive = []
+    Unreachable = []
+    for ip_address in list_ip_addresses:
+        reply = subprocess.run(['ping', '-c', '3', '-n', ip_address], stdout=subprocess.DEVNULL,
+                                   stderr=subprocess.DEVNULL)
+        if reply.returncode == 0:
+             Alive.append(ip_address)
+        else:
+             Unreachable.append(ip_address)
+    return Alive, Unreachable
+
+if __name__ == "__main__":
+    result = check_ip_availability(list_ip_addresses)
+    print('Alive' + str(result[0]))
+    print('Unreachable' + str(result[1]))
